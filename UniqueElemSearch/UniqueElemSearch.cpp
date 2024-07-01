fd0b06e5f3a6dd0a5e13a030ce1ab7200593ad32
MkaGodh <fenryrvla@yandex.ru>
2024-07-01 15:54:44 +0300

    Добавьте файлы проекта.

---
+++ UniqueElemSearch/UniqueElemSearch.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+	std::vector<int> vec = { 1, 1, 1, 5, 5, 7, 4, 6, 1, 5 };
+
+	for (int i = 0; i < vec.size(); i++) {
+		if (vec[i] != -1) {
+			for (int j = i + 1; j < vec.size(); j++) {
+				if (vec[j] == vec[i]) {
+					vec[j] = -1;
+				}
+			}
+		}
+	}
+	int unique = 0;
+	for (int i = 0; i < vec.size(); i++) {
+		if (vec[i] != -1)
+			++unique;
+	}
+
+	std::cout << unique;
+}
